553f25077c5c83dd64d7da67315689bfdc22c08c
QuiD <wodnd101@naver.com>
2022-04-18 17:52:20 +0900

    GET 테스트

--- SpringMVC/src/main/java/com/webmvc/controller/HomeController.java
+++ SpringMVC/src/main/java/com/webmvc/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.webmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,5 +13,12 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
         return "index";
+    }
+
+    @GetMapping(value = "/test")
+    public String test(Model model){
+        model.addAttribute("city","seattle");
+        model.addAttribute("name","Tom");
+        return "home";
     }
 }
---
+++ SpringMVC/target/webmvc-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 2022-04-18
+  Time: 오후 5:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<p>
+    city: ${city}
+    name: ${name}
+</p>
+</body>
+</html>
