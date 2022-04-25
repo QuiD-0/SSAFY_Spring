6d2961282415c7046f0f3876ccaaccc4732d2ef7
QuiD <wodnd101@naver.com>
2022-04-25 13:09:30 +0900

    lombok 적용

--- SpringMVC/src/main/java/com/webmvc/vo/Board.java
+++ SpringMVC/src/main/java/com/webmvc/vo/Board.java
@@ -1,70 +1,31 @@
 package com.webmvc.vo;
 
-//vo(value object):값을 저장하기 위한 용도.테이블 안의 레코드 한 건의 값을 저장하기 위한 목적
-public class Board {
-	private String num;
-	private String pass;
-	private String name;
-	private String wdate;
-	private String title;
-	private String content;
-	private String count;
-	
-	//constructor
-	public Board() {}	
-	
-	public Board(String num, String pass, String name, String wdate, String title, String content, String count) {	
-		this.num = num;
-		this.pass = pass;
-		this.name = name;
-		this.wdate = wdate;
-		this.title = title;
-		this.content = content;
-		this.count = count;
-	}
+import lombok.*;
 
-	//getter/setter
-	public String getNum() {
-		return num;
-	}
-	
-	public void setNum(String num) {
-		this.num = num;
-	}
-	public String getPass() {
-		return pass;
-	}
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getWdate() {
-		return wdate;
-	}
-	public void setWdate(String wdate) {
-		this.wdate = wdate;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getCount() {
-		return count;
-	}
-	public void setCount(String count) {
-		this.count = count;
-	}
+@Getter
+@Setter
+//@NoArgsConstructor   // final이 있을때는 사용못함
+@AllArgsConstructor
+@RequiredArgsConstructor // final, @NonNull이 붙어있는 필드를 arg로 하는 생성자를 생성해준다.
+@EqualsAndHashCode(of = {"num","name"}, callSuper = false) // num,name 만 같으면 같은 객체라고 리턴
+@Data
+public class Board {
+    @NonNull
+    private String num;
+    @NonNull
+    private String pass;
+    private final String name;
+    private String wdate;
+    private String title;
+    private String content;
+    private String count;
+
+    public static void main(String[] args) {
+        Board b1 = new Board("111","111","tommy","2020-04-25","hi","hello world!","0");
+        Board b2 = new Board("111","222","tommy","2020-04-26","hello","hi world!","1");
+        Board b3 = new Board("111","222","tommyCar","2020-04-26","hello","hi world!","1");
+        System.out.println(b1.equals(b2));
+        System.out.println(b1.equals(b3));
+    }
+
 }
