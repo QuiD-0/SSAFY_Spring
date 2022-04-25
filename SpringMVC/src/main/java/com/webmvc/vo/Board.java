695f2649a3ce31cc5af2e4c03b14d725dde9b4ed
QuiD <wodnd101@naver.com>
2022-04-25 13:18:24 +0900

    lombok

--- SpringMVC/src/main/java/com/webmvc/vo/Board.java
+++ SpringMVC/src/main/java/com/webmvc/vo/Board.java
@@ -7,8 +7,9 @@ import lombok.*;
 //@NoArgsConstructor   // final이 있을때는 사용못함
 @AllArgsConstructor
 @RequiredArgsConstructor // final, @NonNull이 붙어있는 필드를 arg로 하는 생성자를 생성해준다.
-@EqualsAndHashCode(of = {"num","name"}, callSuper = false) // num,name 만 같으면 같은 객체라고 리턴
+@EqualsAndHashCode(of = {"num", "name"}, callSuper = false) // num,name 만 같으면 같은 객체라고 리턴
 @Data
+@ToString(includeFieldNames = false)
 public class Board {
     @NonNull
     private String num;
@@ -21,11 +22,13 @@ public class Board {
     private String count;
 
     public static void main(String[] args) {
-        Board b1 = new Board("111","111","tommy","2020-04-25","hi","hello world!","0");
-        Board b2 = new Board("111","222","tommy","2020-04-26","hello","hi world!","1");
-        Board b3 = new Board("111","222","tommyCar","2020-04-26","hello","hi world!","1");
+        Board b1 = new Board("111", "111", "tommy", "2020-04-25", "hi", "hello world!", "0");
+        Board b2 = new Board("111", "222", "tommy", "2020-04-26", "hello", "hi world!", "1");
+        Board b3 = new Board("111", "222", "tommyCar", "2020-04-26", "hello", "hi world!", "1");
         System.out.println(b1.equals(b2));
         System.out.println(b1.equals(b3));
+
+        System.out.println(b1);
     }
 
 }
