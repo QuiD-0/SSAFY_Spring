ed4202e1f211ee95453426ae942966b6791113fa
QuiD <wodnd101@naver.com>
2022-05-07 14:36:40 +0900

    update

--- SpringREST/src/main/java/com/mvc/mapper/CustomerMapper.java
+++ SpringREST/src/main/java/com/mvc/mapper/CustomerMapper.java
@@ -1,19 +1,31 @@
 package com.mvc.mapper;
 
-import java.util.List;
-
 import com.mvc.vo.Customer;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 //client(CustomerServiceImpl.java)
 public interface CustomerMapper {
-	public List<Customer> selectAll();
-	public Customer selectOne(String num);
-	public int insert(Customer c);
-	
-	public int delete(String num);
-	public List<Customer> findByAddress(String address);
-	public int update(Customer c);
-	
+
+    @Select(" select * from customer")
+    public List<Customer> selectAll();
+
+    @Select("select * from customer where num = #{num}")
+    public Customer selectOne(@Param("num") String num);
+
+    @Insert("insert into customer values(#{num}, #{name}, #{address}) ")
+    public int insert(@Param("c") Customer c);
+
+	@Delete("delete from customer where num = #{num}")
+    public int delete(@Param("num") String num);
+
+	@Select("select * from customer where address = #{address}")
+    public List<Customer> findByAddress(@Param("address") String address);
+
+	@Update("delete from customer where num = #{num}")
+    public int update(@Param("c") Customer c);
+
 }
 
 
