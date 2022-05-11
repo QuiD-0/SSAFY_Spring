3e651f5b1642ff9d29a3a63054605d352ebf110b
QuiD <wodnd101@naver.com>
2022-05-11 11:28:06 +0900

    update

--- boot_DB/src/main/java/com/boot_db/BootDbApplication.java
+++ boot_DB/src/main/java/com/boot_db/BootDbApplication.java
@@ -3,9 +3,11 @@ package com.boot_db;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @MapperScan(basePackages = {"com.boot_db.mapper"})
 @SpringBootApplication
+@CrossOrigin("*")
 public class BootDbApplication {
 
     public static void main(String[] args) {
