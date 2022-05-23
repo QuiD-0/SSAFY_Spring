06065c471dc4af7e5602bb86d154c4c860436573
QuiD <wodnd101@naver.com>
2022-05-23 13:52:16 +0900

    Update NewsParseController.java

--- happyhouse/src/main/java/com/happyhouse/controller/NewsParseController.java
+++ happyhouse/src/main/java/com/happyhouse/controller/NewsParseController.java
@@ -1,14 +1,7 @@
 package com.happyhouse.controller;
 
-import com.happyhouse.domain.HouseInfoDto;
 import com.happyhouse.domain.News;
-import com.happyhouse.service.HouseMapService;
 import com.happyhouse.service.NewsService;
-import com.happyhouse.util.Translate;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
