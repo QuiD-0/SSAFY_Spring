abf7b735f757033a2d652730ae7fb94005fad4a3
QuiD <wodnd101@naver.com>
2022-05-24 13:02:33 +0900

    add CrossOrigin

--- happyhouse/src/main/java/com/happyhouse/news/controller/NewsParseController.java
+++ happyhouse/src/main/java/com/happyhouse/news/controller/NewsParseController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/news")
 @Api(value = "News", tags = {"뉴스 크롤링 API"})
+@CrossOrigin("*")
 public class NewsParseController {
 
     @Autowired
--- happyhouse/src/main/java/com/happyhouse/weather/controller/WeatherController.java
+++ happyhouse/src/main/java/com/happyhouse/weather/controller/WeatherController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/weather")
 @Api(value = "Weather", tags = {"날씨 제공 API"})
+@CrossOrigin("*")
 public class WeatherController {
 
     @Autowired
